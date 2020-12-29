b6569053cfcb8648270d39ffbfe49e332b58185d
PatrickDohn <dohnpatrick1993@gmail.com>
2020-12-29 11:19:54 -0500

    adds base structure for cards

--- src/components/Index/Index.js
+++ src/components/Index/Index.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import apiUrl from '../../apiConfig'
+import Card from '../Card/Card'
+import './index.scss'
 
 const Index = () => {
   const [index, setIndex] = useState([])
@@ -13,9 +15,17 @@ const Index = () => {
       .then(res => setIndex(res.data.messages))
   }, [])
 
-  const messageData = index.map(item => <div key={item.id}>{item.content}, {item.name}, {item.clinician}, {item.createdAt}</div>)
+  const messageData = index.map(item => <div key={item.id}>
+    <Card
+      name={item.name}
+      content={item.content}
+      facility={item.facility}
+      clinician={item.clinician}
+      date={item.createdAt}
+    />
+  </div>)
   return (
-    <div>{messageData}</div>
+    <div className="index-container">{messageData}</div>
   )
 }
 

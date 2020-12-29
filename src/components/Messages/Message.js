72822480cf914eba95759da491a2f81bcebaecdc
PatrickDohn <44790175+PatrickDohn@users.noreply.github.com>
2020-12-28 21:44:22 -0500

    message form working (#1)

---
+++ src/components/Messages/Message.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import apiUrl from '../../apiConfig'
+
+const Message = ({ user }) => {
+  const [message, setMessage] = useState({})
+  const [messageId, setMessageId] = useState(null)
+
+  const handleChange = event => {
+    console.log(messageId)
+    event.persist()
+    setMessage(prevMessage => {
+      const updatedMessage = { [event.target.name]: event.target.value }
+      console.log(updatedMessage)
+      const editedMessage = Object.assign({}, prevMessage, updatedMessage)
+      console.log(editedMessage)
+      return editedMessage
+    })
+  }
+
+  const formReset = () => {
+    document.getElementById('message').reset()
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    axios({
+      url: `${apiUrl}/messages/`,
+      method: 'POST',
+      headers: {
+        'Authorization': `Token token=${user.token}`
+      },
+      data: { message }
+    })
+      .then(res => setMessageId(res.data.message._id))
+      .then(setMessage({}))
+      .then(() => formReset())
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} id="message" name="message">
+        <p>Name:</p>
+        <input name="name" onChange={handleChange} placeholder="Name"></input>
+        <p>Message:</p>
+        <textarea name="content" onChange={handleChange} cols={50} rows={5} placeholder=""></textarea>
+        <input name="clinician" onChange={handleChange} placeholder="Clinician"></input>
+        <input name="facility" onChange={handleChange} placeholder="Facilty"></input>
+        <input name="state" onChange={handleChange} placeholder="Location"></input>
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  )
+}
+export default Message

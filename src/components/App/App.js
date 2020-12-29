c069c9b284cac5d0c1a05f771558077afaf9da52
PatrickDohn <dohnpatrick1993@gmail.com>
2020-12-28 21:40:07 -0500

    message form working

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,6 +8,7 @@ import SignUp from '../SignUp/SignUp'
 import SignIn from '../SignIn/SignIn'
 import SignOut from '../SignOut/SignOut'
 import ChangePassword from '../ChangePassword/ChangePassword'
+import Message from '../Messages/Message'
 
 class App extends Component {
   constructor () {
@@ -53,6 +54,9 @@ class App extends Component {
           )} />
           <AuthenticatedRoute user={user} path='/change-password' render={() => (
             <ChangePassword msgAlert={this.msgAlert} user={user} />
+          )} />
+          <AuthenticatedRoute user={user} path='/' render={() => (
+            <Message msgAlert={this.msgAlert} user={user} />
           )} />
         </main>
       </Fragment>

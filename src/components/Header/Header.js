2d3ed727d5ac7aa06bc71b0ff4b9eb4aff5d41c7
PatrickDohn <dohnpatrick1993@gmail.com>
2020-12-30 15:25:44 -0500

    fix and style nav bar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,15 +6,24 @@ import { Form, FormControl } from 'react-bootstrap'
 // Image and Icon imports
 import appLogo from '../../Images/MediLogo.svg'
 import searchIcon from '../../Images/SearchIcon.svg'
+import { MdAddCircle, MdPerson } from 'react-icons/md'
 
 import './header.scss'
 
 const authenticatedOptions = (
   <Fragment>
+    <Nav.Link className="message-link" href="#message">
+      <MdAddCircle className="message-icon" />
+      Message
+    </Nav.Link>
+    <Nav.Link className="message-link" href="#profile">
+      <MdPerson className="message-icon" />
+      Profile
+    </Nav.Link>
     <Nav.Link href="#change-password">Change Password</Nav.Link>
-    <Nav.Link href="#sign-out">Sign Out</Nav.Link>
-    <Nav.Link href="#profile">Profile</Nav.Link>
-    <Nav.Link href="#message">Message</Nav.Link>
+    <Nav.Link className="message-link" href="#sign-out">
+      Sign Out
+    </Nav.Link>
   </Fragment>
 )
 
@@ -24,34 +33,50 @@ const unauthenticatedOptions = (
   </Fragment>
 )
 
-const Header = ({ user, handleChange }) => (
-  <Navbar className="header-nav-bar" expand="md">
-    <div className="search-icon">
-      <img src={searchIcon}></img>
-      <Form inline>
-        <FormControl
-          onChange={handleChange}
-          type="text"
-          placeholder="Search"
-          className="nav-search-bar mr-sm-2"
-        />
-      </Form>
-    </div>
-    <div className="app-logo">
-      <a href="#">
-        <img src={appLogo} alt="Medi thanks logo"></img>
-      </a>
-    </div>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="ml-auto">
-        {user && (
-          <span className="navbar-text mr-2">Welcome, {user.email}</span>
-        )}
-        {user ? authenticatedOptions : unauthenticatedOptions}
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-)
+const Header = ({ user, handleChange }) => {
+  const hideSearchBar = window.location.hash === '#/profile'
+  return (
+    <Navbar className="header-nav-bar" expand="md">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          {user ? authenticatedOptions : unauthenticatedOptions}
+        </Nav>
+      </Navbar.Collapse>
+
+      <div className="app-logo">
+        <a href="#">
+          <img src={appLogo} alt="Medi thanks logo"></img>
+        </a>
+      </div>
+
+      {!hideSearchBar ? (
+        <div className="search-bar-container">
+          <img src={searchIcon}></img>
+          <Form inline>
+            <FormControl
+              onChange={handleChange}
+              type="text"
+              placeholder="Search"
+              className="nav-search-bar mr-sm-2"
+            />
+          </Form>
+        </div>
+      ) : (
+        <div className="hide-search-bar" style={{ display: 'none' }}>
+          <img src={searchIcon}></img>
+          <Form inline>
+            <FormControl
+              onChange={handleChange}
+              type="text"
+              placeholder="Search"
+              className="mr-sm-2"
+            />
+          </Form>
+        </div>
+      )}
+    </Navbar>
+  )
+}
 
 export default Header

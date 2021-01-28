72f11ff99ff8dbb8276b557d9b18e1f40634bba3
Rob <robrichardsdpt@gmail.com>
2021-01-28 14:32:14 -0500

    updated cards to hide the empty hashtags

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -30,13 +30,13 @@ function Card({
         </Button>
           : ''}
       </div>
-      <div className={facility || clinician ? 'top-content' : ''} >
+      <div className={facility !== ' ' || clinician !== ' ' || state !== ' ' ? 'top-content' : ''} >
         <h2 className="content">{content}</h2><br/>
         <h3 className="name">-{name}</h3><br/>
       </div>
-      {facility ? <p className="facility">#{facility}</p> : ''}
-      {clinician ? <p className="clinician">#{clinician}</p> : ''}
-      {state ? <p className="state">#{state}</p> : ''}
+      {facility !== ' ' ? <p className="facility">#{facility}</p> : ''}
+      {clinician !== ' ' ? <p className="clinician">#{clinician}</p> : ''}
+      {state !== ' ' ? <p className="state">#{state}</p> : ''}
       <div>
         <Overlay target={target.current} show={show} placement="right">
           {({ placement, arrowProps, show: _show, popper, ...props }) => (

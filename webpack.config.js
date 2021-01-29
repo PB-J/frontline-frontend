9ae3d51c1fb2a8a84f2c74966b5292b54e9f30d7
David Ko <david.holy.ko@gmail.com>
2021-01-29 17:45:31 -0500

    add custom webpack

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
+const rules = [
+  {
+    test: /\.js$/,
+    loader: 'babel-loader',
+    exclude: /node_modules/
+  },
+  {
+    test: /\.s[ac]ss$/i,
+    use: ['style-loader', 'css-loader', 'sass-loader']
+  },
+  {
+    test: /\.js$/,
+    enforce: 'pre',
+    use: ['source-map-loader']
+  },
+  {
+    test: /\.(webm|webp|mp4|png|svg)$/,
+    loader: 'file-loader'
+  }
+]
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: './public/index.html',
+    favicon: './public/favicon.ico'
+  }),
+  new CopyPlugin({
+    patterns: [
+      {
+        from: 'public/manifest.json',
+        to: 'assets/manifest.json',
+        toType: 'file'
+      }
+    ]
+  })
+]
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules
+  },
+  plugins,
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true
+  }
+}
